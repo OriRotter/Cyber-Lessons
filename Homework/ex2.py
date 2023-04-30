01e91826e9bac473c6c482ac210333182969d621
Ori Rotter <80815454+OriRotter@users.noreply.github.com>
2023-04-30 17:30:50 +0300

    Exercise 2 Files, Functions, Exceptions. morse code

--- Homework/ex2.py
+++ Homework/ex2.py
@@ -2,20 +2,24 @@ import re
 
 MORSE_CODE_DICT = {'.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9', '-----': '0', '--..--': ', ', '.-.-.-': '.', '..--..': '?', '-..-.': '/', '-....-': '-','/':' '}
 def fileCheck(file):
-    with open(file,'r') as f:
-        data = f.read().replace('\n','')
-    f.close()
-    pattern = r'^[-./ ]*$'
-    match = re.match(pattern, data)
-    if not match:
-        print("Error in Morse Code")
-        return
-
-    for word in data.split(' '):
-        if len(word) > 6:
+    try: # בגלל שאני חייב
+        with open(file,'r') as f:
+            data = f.read().replace('\n','')
+        f.close()
+        pattern = r'^[-./ ]*$'
+        match = re.match(pattern, data)
+        if not match:
             print("Error in Morse Code")
             return
-    return data
+
+        for word in data.split(' '):
+            if len(word) > 6:
+                print("Error in Morse Code")
+                return
+        return data
+    except:
+        print("Error in Morse Code")
+        return
 
 def countSymbols(message):
     count = {}
@@ -32,7 +36,7 @@ def countSymbols(message):
 def main():
     # file = input("Enter file name: ")
     # data = fileCheck(f"D:\Desktops\Ori\data\{file}}")
-    data = fileCheck("D:\Desktops\Ori\data\\morse2.txt")
+    data = fileCheck("Data\\ex2\\morse2.txt")
     if(data != None):
         data, count = countSymbols(data)
         print(data)
